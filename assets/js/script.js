3541e3dc3c3a93badacd2cda7b604841ab0a7a9e
johanvivancx <jjona@puce.edu.ec>
2026-03-03 21:21:06 -0500

    AvanzeDemas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,34 @@
-// Seleccionamos todos los botones de productos
-const botones = document.querySelectorAll(".producto button");
+const form = document.getElementById("formulario");
 
-// Recorremos cada botón
-botones.forEach(boton => {
-    boton.addEventListener("click", function() {
-        
-        // Obtenemos el nombre del producto
-        const producto = this.parentElement.querySelector("h3").textContent;
-        
-        // Mostramos mensaje
-        alert("Has agregado al carrito: " + producto);
-    });
+if(form){
+
+form.addEventListener("submit",function(e){
+
+e.preventDefault();
+
+let nombre=document.getElementById("nombre").value.trim();
+let email=document.getElementById("email").value.trim();
+let descripcion=document.getElementById("descripcion").value.trim();
+
+let mensaje=document.getElementById("mensaje");
+
+if(nombre.length<3){
+mensaje.textContent="Nombre mínimo 3 caracteres";
+return;
+}
+
+if(!email.includes("@")){
+mensaje.textContent="Email inválido";
+return;
+}
+
+if(descripcion.length<10){
+mensaje.textContent="Descripción mínima 10 caracteres";
+return;
+}
+
+mensaje.style.color="green";
+mensaje.textContent="Formulario enviado correctamente ✅";
+
 });
+}
